f8c3f45faef3b349c258bb4c046d48a8e19559ac
wazk2008 <49433213+wazk2008@users.noreply.github.com>
2019-07-23 11:28:02 +0800

    Update Cmfz5Application.java

--- src/main/java/com/baizhi/Cmfz5Application.java
+++ src/main/java/com/baizhi/Cmfz5Application.java
@@ -28,6 +28,10 @@ public class Cmfz5Application {
         System.out.println("++++++++++++++++++++++");
         System.out.println("++++++++++++++++++++++");
         System.out.println("++++++++++++++++++++++");
+        System.out.println("？？？？？？？？？？？？");
+        System.out.println("？？？？？？？？？？？？");
+        System.out.println("？？？？？？？？？？？？");
+        
 
         // 1. 需要定义一个converter转换消息的对象
         FastJsonHttpMessageConverter fasHttpMessageConverter = new FastJsonHttpMessageConverter();
